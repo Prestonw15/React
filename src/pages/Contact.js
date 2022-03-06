422d29c09afc70f2c87a3607979bbe11e1e0207a
Prestonw15 <prestonwatson.15@gmail.com>
2022-03-06 13:38:49 -0700

    added the rest of the simple file structure

---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+const Contact = (props) => {
+
+    return(<>
+        <Navbar/>
+        <Footer/>
+        </>
+    )
+}
+
+
+
+export default Contact;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+const Portfolio = (props) => {
+
+    return(<>
+        <Navbar/>
+        <Footer/>
+        </>
+    )
+}
+
+
+
+export default Portfolio;
