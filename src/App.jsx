272d0f643614969561d89e03092786c18238aa27
Prestonw15 <prestonwatson.15@gmail.com>
2022-03-06 13:57:40 -0700

    updated packages and can now deploy the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+
 
 
 function App() {
     return(
         <Router>
         <div>
-            <Route exact path={"/"}><Home/></Route>
+            <Route exact path={"/Home"}><Home/></Route>
             <Route exact path={"/About"}><About/></Route>
             <Route exact path={"/Portfolio"}><Portfolio/></Route>
             <Route exact path={"/Contact"}><Contact/></Route>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => {
     return( <>
     <header>
         <nav>
             <ul>
+                <li><Link to={"/Home"}>Home</Link></li>
                 <li><Link to="/Portfolio">Portfolio</Link> </li>
                 <li><Link to="/Contact">Contact Me</Link> </li>
                 <li><Link to="/About">About Me</Link> </li>
