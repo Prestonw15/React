0c4c9711346e2d81fa5640ca32333ee6bf0020b1
Prestonw15 <prestonwatson.15@gmail.com>
2022-03-06 13:10:11 -0700

    node modules and ABout me pages

---
+++ client/src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../index.css";
+
+const Footer = () => {
+    return( <>
+    <footer>
+        <article>
+            <div> &copy;2021 <strong>Preston's Portfolio&nbsp;</strong></div>
+        </article>
+    </footer>
+    </>
+    )
+}
+
+export default Footer;
---
+++ client/src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+const About = (props) => {
+
+    return(<>
+        <Navbar/>
+        <h1>About Me</h1>
+        <div>
+            I'm a 24 yr old male, who just finsihed studying code at University of Utah! I'm Now a Certified FUll-Stack Developer and can use multiple languages and database to help me build my applications.
+            Some of the things I enjoy are:
+            <ul>
+                <li>Playing Basketball</li>
+                <li>Playing Video Games</li>
+                <li>Spending Time with Family</li>
+                <li>Working Out</li>
+                <li>Spending Time with My Girlfriend</li>
+            </ul>
+            I Also Come from a really big family, I have 9 younger siblings of which I'm the oldest. I goal is to work towards coding and programming in the Video games Field, and would one Day Really like to work for Bethesda Game Studios!
+
+            If you have Any questions about me feel free to checkout the contact Page to reach out.
+        </div>
+        <Footer/>
+    </>
+    )
+}
+
+export default About;
