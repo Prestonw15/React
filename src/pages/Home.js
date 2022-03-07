bc486243a8c9c16c654059ec0c8456b37798b2c1
Prestonw15 <prestonwatson.15@gmail.com>
2022-03-06 18:42:40 -0700

    added more pics and tried to change more styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,7 @@ const Home = (props) => {
     return(<>
         <Navbar/>
         <div style={{textAlign:"center"}}>
+            <img src={require('../images/profile.png').default} alt="profile pic" /> <br/>
             <a href="https://docs.google.com/document/d/1oxoqsUDnObQh2quBDT4kCnPS473Sw28QyVMgdVuL3pU/edit?usp=sharing">Click Here For My Resume</a>
         </div>
         <Footer/>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -8,26 +8,24 @@ const Portfolio = (props) => {
 
     return(<>
         <Navbar/>
-        <div>
-            <a className="Work" href=" https://prestonw15.github.io/RUN-BUDDY/">RUN-BUDDY</a>
-            <img className="Runbuddy" src="./Assets/images/Runbuddy.png" alt="Runbuddy homepage"/>
-        </div>
-        <div>
-            <a className="Work" href="https://freeurmindpw.herokuapp.com/">Free Ur Mind</a>
-            <img className="FreeUrMind" src="./src/images/2022-02-13.png" alt="Free UR Mind Homepage"/>
-        </div>
-        <div>
-            <a className="Work" href="https://thisismyswamp.herokuapp.com/">Budget-Tracker</a>
-            <img className="Budget-Tracker" src="./src/images/2022-02-13.png" alt="Screenshot of Budget-Tracker"/>
-        </div>
-        <div>
-            <a className="Work" href="https://orgesarelikeonions.herokuapp.com/">Tech-Blog</a>
-            <img className="Tech-Blog" src="./src/images/2022-02-13.png" alt="Tech-Blog Homepage screenshot"/>
-        </div>
-        <div>
-            <a className="Work" href="https://battlestats.herokuapp.com/">BATTLESTATS</a>
-            <img className="BATTLESTATS2.0" src="./src/images/2022-02-13.png" alt="BATTLESTATS login page"/>
-        </div>
+        <section style={{backgroundColor:'#9a8f97', height:'100%', display:'flex', justifyContent:'space-evenly'}}>
+            <div>
+                <a className="Work" href=" https://prestonw15.github.io/RUN-BUDDY/">RUN-BUDDY</a>
+                <img className="Runbuddy" src="./Assets/images/Runbuddy.png" alt="Runbuddy homepage"/>
+
+                <a className="Work" href="https://freeurmindpw.herokuapp.com/">Free Ur Mind</a>
+                <img className="FreeUrMind" src="./src/images/freeurmind.png" alt="Free UR Mind Homepage"/>
+
+                <a className="Work" href="https://thisismyswamp.herokuapp.com/">Budget-Tracker</a>
+                <img className="Budget-Tracker" src="./src/images/2022-02-13.png" alt="Screenshot of Budget-Tracker"/>
+           
+                <a className="Work" href="https://orgesarelikeonions.herokuapp.com/">Tech-Blog</a>
+                <img className="Tech-Blog" src="./src/images/2022-02-13.png" alt="Tech-Blog Homepage screenshot"/>
+            
+                <a className="Work" href="https://battlestats.herokuapp.com/">BATTLESTATS</a>
+                <img className="BATTLESTATS2.0" src="./src/images/BATTLESTATS.png" alt="BATTLESTATS login page"/>
+            </div>
+        </section>
         <Footer/>
         </>
     )
